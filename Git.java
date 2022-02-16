5b1839724c46e5e8c50d38ce3673ad0123b52921
pajajandro <danielvivarvivaralejandro@gmail.com>
2022-02-16 19:37:50 +0100

    1r commit

---
+++ Git.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author TANIA
+ */
+public class Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+          String paraules = " ";
+          paraules=sc.nextLine();
+          
+         System.out.println("Muy bien hecho payaso: " + paraules);
+          }
+          
+    }
+    
+
